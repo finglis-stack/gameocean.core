756d2b28572419075ffe806c7b4f6437a0a1b1ec
finglis-stack <finglis@sivara.ca>
2026-04-04 20:42:59 -0400

    chore: remove build.bat from repo and add to gitignore

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SET JAVA_HOME=C:\Users\felix\.jdks\ms-21.0.8
-SET PATH=%JAVA_HOME%\bin;%PATH%
-C:\tools\maven\apache-maven-3.9.14\bin\mvn.cmd clean package > build_output.txt 2>&1
-echo Build exit code: %ERRORLEVEL% >> build_output.txt
